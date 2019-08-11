df32d24cb44d918019e57ac0b4fc2be0d65dd22b
David Rivera <David.R.Rivera193@gmail.com>
2019-08-11 13:05:08 -0700

    move stuff around

--- interface/web/src/App.js
+++ interface/web/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Clock from './components/Clock';
+import Tempreature from './components/Tempreature';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Clock/>
+        <Tempreature/>
       </header>
     </div>
   );
---
+++ interface/web/src/components/Tempreature.js
@@ -0,0 +1,28 @@
+    
+import React, { Component } from "react";
+
+
+export default class Tempreature extends Component {
+
+//   state = {
+//     time: new Date().toLocaleString(),
+//   };
+
+//   componentDidMount() {
+//     this.intervalID = setInterval(
+//       () => this.tick(),
+//       1000
+//     );
+//   }
+//   tick() {
+//     this.setState({
+//       time: new Date().toLocaleString()
+//     });
+//   }
+
+  render() {
+    return (
+      <span>temp</span>
+    );
+  }
+}
