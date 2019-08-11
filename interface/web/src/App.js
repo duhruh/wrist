9b8e397df59167119400f3a7925498d0e2d7a336
David Rivera <David.R.Rivera193@gmail.com>
2019-08-11 13:34:26 -0700

    k

--- interface/web/src/App.js
+++ interface/web/src/App.js
@@ -4,15 +4,37 @@ import './App.css';
 import Clock from './components/Clock';
 import Tempreature from './components/Tempreature';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Clock/>
-        <Tempreature/>
-      </header>
-    </div>
-  );
+
+import {TempreatureService} from './lib/tempreature/temperature_grpc_web_pb';
+import {ReadingsRequest} from './lib/tempreature/temperature_pb';
+
+class App extends Component {
+state = {
+  client: new TempreatureService(process.env.REACT_APP_TEMPREATURE_API),
+}
+
+componentDidMount(){
+  let req = new ReadingsRequest();
+  let stream = this.state.client.streamReadings(req);
+  stream.on('data', this.onReading.bind(this));
+  stream.on('end', ()=>{
+    console.log("done")
+    console.log(arguments)
+  })
+}
+onReading(){
+  console.log(arguments);
+}
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Clock/>
+          <Tempreature/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
